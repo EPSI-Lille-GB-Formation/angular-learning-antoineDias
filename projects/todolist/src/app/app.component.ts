138b57c739b16fd410fe25b98d1cd3fa10cf43b4
Antoine Dias <antoine.dias.dunkerque@gmail.com>
2023-11-17 15:05:31 +0100

    TP3

--- projects/todolist/src/app/app.component.ts
+++ projects/todolist/src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { TODOS } from './mock-todo';
 
 @Component({
   selector: 'app-root',
-  template: '<h1>Hello world!</h1>',
+  template: `
+    <h1>Hello world!!</h1>
+    <ul>
+      <li>{{ todoList[0].title }}</li>
+      <li>{{ todoList[1].title }}</li>
+      <li>{{ todoList[2].title }}</li>
+      <li>{{ todoList[3].title }}</li>
+      <li>{{ todoList[4].title }}</li>
+      <li>{{ todoList[5].title }}</li>
+      <li>{{ todoList[6].title }}</li>
+      <li>{{ todoList[7].title }}</li>
+      <li>{{ todoList[8].title }}</li>
+      <li>{{ todoList[9].title }}</li>*
+    </ul>
+  `,
   styleUrls: []
 })
+
+
 export class AppComponent {
-  title = 'todolist';
+
+  todoList = TODOS;
+
+  constructor() {}
+
+  ngOnInit(){
+    console.table(this.todoList);
+    this.selectTodo(8);
+  }
+
+  selectTodo(id: number){
+    console.table(this.todoList[id-1]);
+  }
+
 }
---
+++ projects/todolist/src/app/mock-todo.ts
@@ -0,0 +1,94 @@
+import { Todo } from "./todo";
+
+export const TODOS: Todo [] = [
+    {
+        id: 1,
+        title: "Todo 1",
+        content: "content todo 1",
+        author: "John Doe",
+        isCompleted: false,
+        createdAt: new Date(),
+        completedAt: null
+    },
+    {
+        id: 2,
+        title: "Todo 2",
+        content: "content todo 2",
+        author: "John Deux",
+        isCompleted: false,
+        createdAt: new Date(),
+        completedAt: null
+    },
+    {
+        id: 3,
+        title: "Todo 3",
+        content: "content todo 3",
+        author: "John Trois",
+        isCompleted: false,
+        createdAt: new Date(),
+        completedAt: null
+    },
+    {
+        id: 4,
+        title: "Todo 4",
+        content: "content todo 4",
+        author: "John Quattre",
+        isCompleted: false,
+        createdAt: new Date(),
+        completedAt: null
+    },
+    {
+        id: 5,
+        title: "Todo 5",
+        content: "content todo 5",
+        author: "John Cinq",
+        isCompleted: false,
+        createdAt: new Date(),
+        completedAt: null
+    },
+    {
+        id: 6,
+        title: "Todo 6",
+        content: "content todo 6",
+        author: "John Six",
+        isCompleted: false,
+        createdAt: new Date(),
+        completedAt: null
+    },
+    {
+        id: 7,
+        title: "Todo 7",
+        content: "content todo 7",
+        author: "John Sept",
+        isCompleted: false,
+        createdAt: new Date(),
+        completedAt: null
+    },
+    {
+        id: 8,
+        title: "Todo 8",
+        content: "content todo 8",
+        author: "John Huit",
+        isCompleted: false,
+        createdAt: new Date(),
+        completedAt: null
+    },
+    {
+        id: 9,
+        title: "Todo 9",
+        content: "content todo 9",
+        author: "John Neuf",
+        isCompleted: false,
+        createdAt: new Date(),
+        completedAt: null
+    },
+    {
+        id: 10,
+        title: "Todo 10",
+        content: "content todo 10",
+        author: "John Dix",
+        isCompleted: false,
+        createdAt: new Date(),
+        completedAt: null
+    }
+]
