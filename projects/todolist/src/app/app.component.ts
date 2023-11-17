9100c70d9fde65a3f976fa3cae1fb6a964d093e2
Antoine Dias <antoine.dias.dunkerque@gmail.com>
2023-11-17 13:48:31 +0100

    Premier commit

---
+++ projects/todolist/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<h1>Hello world!</h1>',
+  styleUrls: []
+})
+export class AppComponent {
+  title = 'todolist';
+}
